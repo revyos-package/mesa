da338b673515dfb86cb0fd3d0a16d8b8cef5cc20
Frank Binns <frank.binns@imgtec.com>
2017-12-22 17:17:50 +0000

    dri: add support for YUV DRI config
    This is prerequisite for adding support for EGL_EXT_yuv_surface.
    
    This also adds support for NV12 and NV21 EGL configs.
    
    Signed-off-by: Randolph Sapp <rs@ti.com>

--- src/gallium/frontends/dri/dri_screen.c
+++ src/gallium/frontends/dri/dri_screen.c
@@ -133,6 +133,21 @@ dri_loader_get_cap(struct dri_screen *screen, enum dri_loader_cap cap)
  *                          This forces 32-bit color to have 24-bit depth, and
  *                          16-bit color to have 16-bit depth.
  *
+ * \param yuv_depth_range YUV pixel depth range. For non-YUV pixel formats this
+ *                        should be \c __DRI_ATTRIB_YUV_DEPTH_RANGE_NONE.
+ *                        Otherwise valid values are
+ *                        \c __DRI_ATTRIB_YUV_DEPTH_RANGE_LIMITED_BIT and
+ *                        \c __DRI_ATTRIB_YUV_DEPTH_RANGE_FULL_BIT. See the
+ *                        EGL_EXT_yuv_surface extension spec for more details.
+ * \param yuv_csc_standard YUV color conversion standard. For non-YUV pixel
+ *                         formats this should be
+ *                         \c __DRI_ATTRIB_YUV_CSC_STANDARD_NONE. Otherwise
+ *                         valid values are
+ *                         \c __DRI_ATTRIB_YUV_CSC_STANDARD_601_BIT,
+ *                         \c __DRI_ATTRIB_YUV_CSC_STANDARD_709_BIT and
+ *                         \c __DRI_ATTRIB_YUV_CSC_STANDARD_2020_BIT. See the
+ *                         EGL_EXT_yuv_surface extension spec for more details.
+ *
  * \returns
  * Pointer to any array of pointers to the \c __DRIconfig structures created
  * for the specified formats.  If there is an error, \c NULL is returned.
@@ -145,7 +160,8 @@ driCreateConfigs(mesa_format format,
                  unsigned num_depth_stencil_bits,
                  const bool *db_modes, unsigned num_db_modes,
                  const uint8_t * msaa_samples, unsigned num_msaa_modes,
-                 GLboolean enable_accum, GLboolean color_depth_match)
+                 GLboolean enable_accum, GLboolean color_depth_match,
+                 GLint yuv_depth_range, GLint yuv_csc_standard)
 {
    static const struct {
       uint32_t masks[4];
@@ -196,6 +212,9 @@ driCreateConfigs(mesa_format format,
       /* MESA_FORMAT_R4G4B4A4_UNORM */
       {{ 0x0000000F, 0x000000F0, 0x00000F00, 0x0000F000 },
        { 0, 4, 8, 12 }},
+      /* Mesa YUV formats */
+      {{ 0, 0, 0, 0 },
+       { -1, -1, -1, -1}},
    };
 
    const uint32_t * masks;
@@ -209,6 +228,11 @@ driCreateConfigs(mesa_format format,
    int green_bits;
    int blue_bits;
    int alpha_bits;
+   int yuv_order = __DRI_ATTRIB_YUV_ORDER_NONE;
+   int yuv_num_planes = 0;
+   int yuv_subsample = __DRI_ATTRIB_YUV_SUBSAMPLE_NONE;
+   int yuv_plane_bpp = __DRI_ATTRIB_YUV_PLANE_BPP_NONE;
+   bool is_yuv = false;
    bool is_srgb;
    bool is_float;
 
@@ -277,6 +301,78 @@ driCreateConfigs(mesa_format format,
       masks = format_table[14].masks;
       shifts = format_table[14].shifts;
       break;
+   case MESA_FORMAT_YCBCR:
+      masks = format_table[15].masks;
+      shifts = format_table[15].shifts;
+      is_yuv = true; /* FIXME: This should come from formats_info.py */
+      yuv_order = __DRI_ATTRIB_YUV_ORDER_YUYV_BIT;
+      yuv_num_planes = 1;
+      yuv_subsample = __DRI_ATTRIB_YUV_SUBSAMPLE_4_2_2_BIT;
+      yuv_plane_bpp = __DRI_ATTRIB_YUV_PLANE_BPP_8_BIT;
+      break;
+   case MESA_FORMAT_YUV420_2PLANE:
+      masks = format_table[15].masks;
+      shifts = format_table[15].shifts;
+      is_yuv = true; /* FIXME: This should come from formats_info.py */
+      yuv_order = __DRI_ATTRIB_YUV_ORDER_YUV_BIT;
+      yuv_num_planes = 2;
+      yuv_subsample = __DRI_ATTRIB_YUV_SUBSAMPLE_4_2_0_BIT;
+      yuv_plane_bpp = __DRI_ATTRIB_YUV_PLANE_BPP_8_BIT;
+      break;
+   case MESA_FORMAT_YVU420_2PLANE:
+      masks = format_table[15].masks;
+      shifts = format_table[15].shifts;
+      is_yuv = true; /* FIXME: This should come from formats_info.py */
+      yuv_order = __DRI_ATTRIB_YUV_ORDER_YVU_BIT;
+      yuv_num_planes = 2;
+      yuv_subsample = __DRI_ATTRIB_YUV_SUBSAMPLE_4_2_0_BIT;
+      yuv_plane_bpp = __DRI_ATTRIB_YUV_PLANE_BPP_8_BIT;
+      break;
+   case MESA_FORMAT_YUV420_3PLANE:
+      masks = format_table[15].masks;
+      shifts = format_table[15].shifts;
+      is_yuv = true; /* FIXME: This should come from formats_info.py */
+      yuv_order = __DRI_ATTRIB_YUV_ORDER_YUV_BIT;
+      yuv_num_planes = 3;
+      yuv_subsample = __DRI_ATTRIB_YUV_SUBSAMPLE_4_2_0_BIT;
+      yuv_plane_bpp = __DRI_ATTRIB_YUV_PLANE_BPP_8_BIT;
+      break;
+   case MESA_FORMAT_YVU420_3PLANE:
+      masks = format_table[15].masks;
+      shifts = format_table[15].shifts;
+      is_yuv = true; /* FIXME: This should come from formats_info.py */
+      yuv_order = __DRI_ATTRIB_YUV_ORDER_YVU_BIT;
+      yuv_num_planes = 3;
+      yuv_subsample = __DRI_ATTRIB_YUV_SUBSAMPLE_4_2_0_BIT;
+      yuv_plane_bpp = __DRI_ATTRIB_YUV_PLANE_BPP_8_BIT;
+      break;
+   case MESA_FORMAT_YCBCR_REV:
+      masks = format_table[15].masks;
+      shifts = format_table[15].shifts;
+      is_yuv = true; /* FIXME: This should come from formats_info.py */
+      yuv_order = __DRI_ATTRIB_YUV_ORDER_UYVY_BIT;
+      yuv_num_planes = 1;
+      yuv_subsample = __DRI_ATTRIB_YUV_SUBSAMPLE_4_2_2_BIT;
+      yuv_plane_bpp = __DRI_ATTRIB_YUV_PLANE_BPP_8_BIT;
+      break;
+   case MESA_FORMAT_VYUY:
+      masks = format_table[15].masks;
+      shifts = format_table[15].shifts;
+      is_yuv = true; /* FIXME: This should come from formats_info.py */
+      yuv_order = __DRI_ATTRIB_YUV_ORDER_VYUY_BIT;
+      yuv_num_planes = 1;
+      yuv_subsample = __DRI_ATTRIB_YUV_SUBSAMPLE_4_2_2_BIT;
+      yuv_plane_bpp = __DRI_ATTRIB_YUV_PLANE_BPP_8_BIT;
+      break;
+   case MESA_FORMAT_YVYU:
+      masks = format_table[15].masks;
+      shifts = format_table[15].shifts;
+      is_yuv = true; /* FIXME: This should come from formats_info.py */
+      yuv_order = __DRI_ATTRIB_YUV_ORDER_YVYU_BIT;
+      yuv_num_planes = 1;
+      yuv_subsample = __DRI_ATTRIB_YUV_SUBSAMPLE_4_2_2_BIT;
+      yuv_plane_bpp = __DRI_ATTRIB_YUV_PLANE_BPP_8_BIT;
+      break;
    default:
       fprintf(stderr, "[%s:%u] Unknown framebuffer type %s (%d).\n",
               __func__, __LINE__,
@@ -332,7 +428,11 @@ driCreateConfigs(mesa_format format,
                     modes->greenShift = shifts[1];
                     modes->blueShift  = shifts[2];
                     modes->alphaShift = shifts[3];
-                    modes->rgbBits   = modes->redBits + modes->greenBits
+
+                    if (is_yuv)
+                        modes->rgbBits = 8;
+                    else
+                        modes->rgbBits = modes->redBits + modes->greenBits
                             + modes->blueBits + modes->alphaBits;
 
                     modes->accumRedBits   = 16 * j;
@@ -343,11 +443,20 @@ driCreateConfigs(mesa_format format,
                     modes->stencilBits = stencil_bits[k];
                     modes->depthBits = depth_bits[k];
 
+                    modes->rgbMode = !is_yuv;
+
                     modes->doubleBufferMode = db_modes[i];
 
                     modes->samples = msaa_samples[h];
 
                     modes->sRGBCapable = is_srgb;
+
+                    modes->YUVOrder = yuv_order;
+                    modes->YUVNumberOfPlanes = yuv_num_planes;
+                    modes->YUVSubsample = yuv_subsample;
+                    modes->YUVDepthRange = yuv_depth_range;
+                    modes->YUVCSCStandard = yuv_csc_standard;
+                    modes->YUVPlaneBPP = yuv_plane_bpp;
                 }
             }
         }
@@ -559,7 +668,9 @@ dri_fill_in_modes(struct dri_screen *screen)
                                         depth_buffer_factor,
                                         db_modes, ARRAY_SIZE(db_modes),
                                         msaa_modes, 1,
-                                        GL_TRUE, !mixed_color_depth);
+                                        GL_TRUE, !mixed_color_depth,
+                                        __DRI_ATTRIB_YUV_DEPTH_RANGE_NONE,
+                                        __DRI_ATTRIB_YUV_CSC_STANDARD_NONE);
          configs = driConcatConfigs(configs, new_configs);
 
          /* Multi-sample configs without an accumulation buffer. */
@@ -569,7 +680,9 @@ dri_fill_in_modes(struct dri_screen *screen)
                                            depth_buffer_factor,
                                            db_modes, ARRAY_SIZE(db_modes),
                                            msaa_modes+1, num_msaa_modes-1,
-                                           GL_FALSE, !mixed_color_depth);
+                                           GL_FALSE, !mixed_color_depth,
+                                           __DRI_ATTRIB_YUV_DEPTH_RANGE_NONE,
+                                           __DRI_ATTRIB_YUV_CSC_STANDARD_NONE);
             configs = driConcatConfigs(configs, new_configs);
          }
       }
