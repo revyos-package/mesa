9349f844750856dcf39b41f886a00a4ad6d1ac10
Silvestrs Timofejevs <silvestrs.timofejevs@imgtec.com>
2018-08-30 13:48:53 +0100

    gbm: add some new GBM formats
    GBM_FORMAT_ARGB4444
    GBM_FORMAT_BGR888
    GBM_FORMAT_RGB888
    GBM_FORMAT_AXBXGXRX106106106106
    GBM_FORMAT_YUYV
    GBM_FORMAT_YVU444_PACK10_IMG
    
    Signed-off-by: Randolph Sapp <rs@ti.com>

--- src/gbm/backends/dri/gbm_dri.c
+++ src/gbm/backends/dri/gbm_dri.c
@@ -406,6 +406,11 @@ static const struct gbm_dri_visual gbm_dri_visuals_table[] = {
      { 5, 5, 5, 1 },
    },
    {
+      GBM_FORMAT_ARGB4444, __DRI_IMAGE_FORMAT_ARGB4444,
+      { 8, 4, 0, 12 },
+      { 4, 4, 4, 4 },
+   },
+   {
      GBM_FORMAT_RGB565, __DRI_IMAGE_FORMAT_RGB565,
      { 11, 5, 0, -1 },
      { 5, 6, 5, 0 },
@@ -429,6 +434,16 @@ static const struct gbm_dri_visual gbm_dri_visuals_table[] = {
      GBM_FORMAT_ABGR8888, __DRI_IMAGE_FORMAT_ABGR8888,
      { 0, 8, 16, 24 },
      { 8, 8, 8, 8 },
+   },
+   {
+     GBM_FORMAT_BGR888, __DRI_IMAGE_FORMAT_BGR888,
+     { 0, 8, 16, -1 },
+     { 8, 8, 8, 0 },
+   },
+   {
+     GBM_FORMAT_RGB888, __DRI_IMAGE_FORMAT_RGB888,
+     { 16, 8, 0, -1 },
+     { 8, 8, 8, 0 },
    },
    {
      GBM_FORMAT_XRGB2101010, __DRI_IMAGE_FORMAT_XRGB2101010,
@@ -461,6 +476,11 @@ static const struct gbm_dri_visual gbm_dri_visuals_table[] = {
      { 16, 16, 16, 16 },
    },
    {
+     GBM_FORMAT_AXBXGXRX106106106106, __DRI_IMAGE_FORMAT_AXBXGXRX106106106106,
+     { 6, 22, 38, 54 },
+     { 10, 10, 10, 10 },
+   },
+   {
      GBM_FORMAT_XBGR16161616F, __DRI_IMAGE_FORMAT_XBGR16161616F,
      { 0, 16, 32, -1 },
      { 16, 16, 16, 0 },
@@ -471,6 +491,12 @@ static const struct gbm_dri_visual gbm_dri_visuals_table[] = {
      { 0, 16, 32, 48 },
      { 16, 16, 16, 16 },
      true,
+   },
+   {
+     GBM_FORMAT_YUYV, __DRI_IMAGE_FORMAT_YUYV,
+   },
+   {
+     GBM_FORMAT_YVU444_PACK10_IMG, __DRI_IMAGE_FORMAT_YVU444_PACK10_IMG,
    },
 };
 
