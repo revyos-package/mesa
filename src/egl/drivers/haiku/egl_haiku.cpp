7377f01a3b4cacff1d41317f769e795eb7af4948
Brendan King <Brendan.King@imgtec.com>
2020-02-10 09:23:03 +0000

    egl: query the supported ES2 context version
    For OpenGL ES contexts, the EGL specification states that querying
    EGL_CONTEXT_CLIENT_VERSION with eglQueryContext may return a version
    that differs from that specified at context creation time. For example,
    if an OpenGL ES2 context is specified at context creation time, an
    OpenGL ES3 context may be created, and so "3" should be returned
    when EGL_CONTEXT_CLIENT_VERSION is queried.
    
    A new EGL driver API function has been added,
    QueryContextClientVersion, that is called when the context client
    version is queried, allowing EGL drivers to override the default
    value (i.e. the version specified at context creation time). If the
    function returns zero, the default is used.
    
    For DRI drivers, QueryContextClientVersion returns zero for all API
    contexts other than OpenGL ES2. For OpenGL ES2, the supported context
    client version is queried via the Query Renderer driver extension, using
    integer query __DRI2_RENDERER_OPENGL_ES2_CONTEXT_CLIENT_VERSION_IMG. If
    the query isn't supported, or the query returns zero, zero is returned
    to the caller.
    
    This change uses dri2_renderer_query_integer, which was removed
    by commit 5ff8edaa2a161da34c7efab36a7b9810dccc27ed, but restored
    by another IMG patch.
    
    IMG NOTE: In order to avoid potential name and value clashes, "_IMG"
    has been added to the end of the new query name, this should be removed
    if an attempt is made to push this patch upstream. The value of the new
    query should be adjusted to be the next one in sequence, rather than the
    large value it currently has.
    
    Signed-off-by: Randolph Sapp <rs@ti.com>

--- src/egl/drivers/haiku/egl_haiku.cpp
+++ src/egl/drivers/haiku/egl_haiku.cpp
@@ -493,12 +493,20 @@ haiku_make_current(_EGLDisplay *disp, _EGLSurface *dsurf, _EGLSurface *rsurf,
    return EGL_TRUE;
 }
 
+static EGLint
+haiku_dri2_query_context_client_version(_EGLDisplay *disp, _EGLContext *ctx)
+{
+   /* Tell caller to use the default value */
+   return 0;
+}
+
 extern "C" const _EGLDriver _eglDriver = {
    .Initialize = haiku_initialize,
    .Terminate = haiku_terminate,
    .CreateContext = haiku_create_context,
    .DestroyContext = haiku_destroy_context,
    .MakeCurrent = haiku_make_current,
+   .QueryContextClientVersion = haiku_dri2_query_context_client_version,
    .CreateWindowSurface = haiku_create_window_surface,
    .CreatePixmapSurface = haiku_create_pixmap_surface,
    .CreatePbufferSurface = haiku_create_pbuffer_surface,
