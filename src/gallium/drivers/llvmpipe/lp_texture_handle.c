e720b23aaf18b1e01dcb3982e0f30d229b13c10b
Yukari Chiba <i@0x7f.cc>
2024-04-16 11:03:11 +0800

    llvmpipe: add an implementation with llvm orcjit
    Signed-off-by: Han Gao <gaohan@iscas.ac.cn>

--- src/gallium/drivers/llvmpipe/lp_texture_handle.c
+++ src/gallium/drivers/llvmpipe/lp_texture_handle.c
@@ -201,14 +201,15 @@ llvmpipe_sampler_matrix_destroy(struct llvmpipe_context *ctx)
 }
 
 static void *
-compile_function(struct llvmpipe_context *ctx, struct gallivm_state *gallivm, LLVMValueRef function,
+compile_function(struct llvmpipe_context *ctx, struct gallivm_state *gallivm, LLVMValueRef function, const char *func_name,
                  bool needs_caching,
                  uint8_t cache_key[SHA1_DIGEST_LENGTH])
 {
    gallivm_verify_function(gallivm, function);
    gallivm_compile_module(gallivm);
 
-   void *function_ptr = func_to_pointer(gallivm_jit_function(gallivm, function));
+   void *function_ptr = func_to_pointer(gallivm_jit_function(gallivm, function, 
+                                                            func_name));
 
    if (needs_caching)
       lp_disk_cache_insert_shader(llvmpipe_screen(ctx->pipe.screen), gallivm->cache, cache_key);
@@ -336,7 +337,7 @@ compile_image_function(struct llvmpipe_context *ctx, struct lp_static_texture_st
 
    free(image_soa);
 
-   return compile_function(ctx, gallivm, function, needs_caching, cache_key);
+   return compile_function(ctx, gallivm, function, "image", needs_caching, cache_key);
 }
 
 static void *
@@ -484,7 +485,7 @@ compile_sample_function(struct llvmpipe_context *ctx, struct lp_static_texture_s
 
    free(sampler_soa);
 
-   return compile_function(ctx, gallivm, function, needs_caching, cache_key);
+   return compile_function(ctx, gallivm, function, "sample", needs_caching, cache_key);
 }
 
 static void *
@@ -566,7 +567,7 @@ compile_size_function(struct llvmpipe_context *ctx, struct lp_static_texture_sta
 
    free(sampler_soa);
 
-   return compile_function(ctx, gallivm, function, needs_caching, cache_key);
+   return compile_function(ctx, gallivm, function, "size", needs_caching, cache_key);
 }
 
 static void
