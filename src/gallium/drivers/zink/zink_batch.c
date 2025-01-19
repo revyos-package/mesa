30e9765074c326c292eb6e0a90e0c496d63e4148
Icenowy Zheng <uwu@icenowy.me>
2025-01-19 09:00:21 +0800

    zink: skip empty VkSubmitInfo when submitting
    The closed blob from Imagination for their Rogue GPUs cannot accept
    empty VkSubmitInfo, skip them.
    
    Signed-off-by: Icenowy Zheng <uwu@icenowy.me>
    Signed-off-by: Han Gao <gaohan@iscas.ac.cn>

--- src/gallium/drivers/zink/zink_batch.c
+++ src/gallium/drivers/zink/zink_batch.c
@@ -671,6 +671,13 @@ submit_queue(void *data, void *gdata, int thread_index)
       }
    }
 
+   if (si[ZINK_SUBMIT_WAIT_ACQUIRE].waitSemaphoreCount != 0 &&
+       si[ZINK_SUBMIT_WAIT_FD].waitSemaphoreCount == 0) {
+      si[ZINK_SUBMIT_WAIT_FD] = si[ZINK_SUBMIT_WAIT_ACQUIRE];
+      num_si--;
+      submit++;
+   }
+
    /* then the real submit */
    si[ZINK_SUBMIT_CMDBUF].waitSemaphoreCount = util_dynarray_num_elements(&bs->wait_semaphores, VkSemaphore);
    si[ZINK_SUBMIT_CMDBUF].pWaitSemaphores = bs->wait_semaphores.data;
